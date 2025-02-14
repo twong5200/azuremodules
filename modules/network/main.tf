4d0215dd7150578582d13850d3d6e2640e95452d
twong5200 <twong5200@gmail.com>
2025-02-14 02:33:44 +0000

    initial fix#7

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   count               = length(var.subnets)
   name                = var.subnets[count.index].name
-  address_prefix      = var.subnets[count.index].address_prefix
+  address_prefixes    = [var.subnets[count.index].address_prefix]
   resource_group_name = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
