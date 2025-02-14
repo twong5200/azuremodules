637dba8aebecca82daa8af70515fd9aea4703696
twong5200 <twong5200@gmail.com>
2025-02-14 02:30:59 +0000

    initial fix#6

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,18 +1,14 @@
-resource "azurerm_network_security_group" "nsg" {
-  name                = var.nsg_name
-  location           = var.location
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
   resource_group_name = var.resource_group_name
-  tags               = var.tags
+}
 
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+resource "azurerm_subnet" "subnet" {
+  count               = length(var.subnets)
+  name                = var.subnets[count.index].name
+  address_prefix      = var.subnets[count.index].address_prefix
+  resource_group_name = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
